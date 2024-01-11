52a8dd20d3b78c0f9c2281e69bae0ec8a5a5342f
Marven <woshitao@126.com>
2024-01-11 10:00:12 +0800

    美化login页面

---
+++ Vo/LoginVo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorLearWebApp.Vo;
+
+public class LoginVo
+{
+    [Required(ErrorMessage = "用户名不能为空")]
+    public string? UserName { get; set; }
+    [Required(ErrorMessage = "密码不能为空")]
+    public string? Password { get; set; }
+    public bool RememberMe { get; set; }
+}
